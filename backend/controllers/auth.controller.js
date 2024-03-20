af08ad2b64f16bea0e230347ec6008efe58cea71
Divyanshu766 <divyanshu.rawat.dev@gmail.com>
2024-03-20 15:55:01 +0530

    Signup functionality with jwt token

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,52 @@
+import generateTokenAndSetCookie from "../utils/generateToken.js";
+import User from "./../models/user.model.js";
+import bcryptjs from "bcryptjs";
 export const signup = async (req, res) => {
   try {
     const { fullName, userName, password, confirmpassword, gender } = req.body;
-  } catch (error) {}
+
+    if (password !== confirmpassword) {
+      return res.status(400).json({ error: "Passwords don't match" });
+    }
+
+    const user = await User.findOne({ userName });
+
+    if (user) {
+      return res.status(400).json({ error: "Username already exist" });
+    }
+
+    // Hashed password
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${userName}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${userName}`;
+
+    const newUser = new User({
+      fullName,
+      userName,
+      password: hashedPassword,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+
+    if (newUser) {
+      //Generate token
+      generateTokenAndSetCookie(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        userName: newUser.userName,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ error: "Invalid user data" });
+    }
+  } catch (error) {
+    console.log("Error in Signup" + error);
+  }
 };
 
 export const login = (req, res) => {
