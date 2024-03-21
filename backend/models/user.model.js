f1cdb63189106ee0ef160a1fb2bde04ae8262dd6
Divyanshu766 <divyanshu.rawat.dev@gmail.com>
2024-03-21 12:16:39 +0530

    Messages and conversation Schema

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,27 +1,30 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  fullName: { type: String, required: true },
-  userName: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    fullName: { type: String, required: true },
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+      enum: ["male", "female"],
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
   },
-  password: {
-    type: String,
-    required: true,
-    minlength: true,
-  },
-  gender: {
-    type: String,
-    required: true,
-    enum: ["male", "female"],
-  },
-  profilePic: {
-    type: String,
-    default: "",
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
