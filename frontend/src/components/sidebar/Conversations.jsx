235cc2972e52b34696431e87c0fe28da0d92ba82
Divyanshu766 <divyanshu.rawat.dev@gmail.com>
2024-03-27 13:52:05 +0530

    Get Conversations

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,14 +1,21 @@
+import useGetConversation from "../../hooks/useGetConversation";
 import Conversation from "./Conversation";
 
 const Conversations = () => {
+  const { loading, conversations } = useGetConversation();
+  console.log(conversations);
   return (
     <div className="py-2 flex flex-col overflow-auto">
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
+      {conversations.map((conversation, index) => {
+        return (
+          <Conversation
+            key={conversation._id}
+            conversation={conversation}
+            lastIdx={index === conversations.length - 1}
+          />
+        );
+      })}
+      {loading ? <span className="loading loading-spinner"></span> : null}
     </div>
   );
 };
